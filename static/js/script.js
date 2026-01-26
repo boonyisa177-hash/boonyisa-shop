013d9ef403419d45865fab8f35610c7e9ecd598f
จิมิจุ้ย <kkartoon552@gmail.com>
2026-01-26 14:42:55 +0700

    update

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+// Custom JavaScript for the e-commerce site
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any client-side functionality here
+    console.log('Boonyisa Shop loaded');
+});
