7a9decac14f2897b0d27b794f485e7d5337c5ffb
boonyisa177-hash <boonyisa177@gmail.com>
2026-02-16 12:05:30 +0700

    save

--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-// Custom JavaScript for the e-commerce site
-document.addEventListener('DOMContentLoaded', function() {
-    // Add any client-side functionality here
-    console.log('Boonyisa Shop loaded');
-});
